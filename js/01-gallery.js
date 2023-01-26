f6d1c36520e0af7ff55b864c9fbc607d168ee1a2
Yurii Soroka <yurisoroka165@gmail.com>
2023-01-26 17:27:26 +0200

    01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,62 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const galleryContainer = document.querySelector('.gallery');
+
+function galleryItemsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div>
+            `;}).join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup(galleryItems));
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////// 
+
+galleryContainer.addEventListener('click', onGalleryItemsClick);
+
+function onGalleryItemsClick(event) {
+    event.preventDefault();
+    const clickedImage = event.target;
+
+    if (clickedImage.nodeName === 'IMG') {
+        openImageInModal(clickedImage.dataset.source);
+    }
+}
+
+function openImageInModal(imageSource) {
+    const instance = basicLightbox.create(`<img src="${imageSource}" width="800" height="600">`);
+    instance.show();
+    window.addEventListener('keydown', onEscButtonPressed);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function onEscButtonPressed(event) {
+    if (event.code === 'Escape') {
+        window.removeEventListener('keydown', onEscButtonPressed);
+        closeModalHandler();
+    }
+}
+
+function closeModalHandler() {
+    const openedModal = document.querySelector('div.basicLightbox');
+    openedModal.classList.remove('basicLightbox--visible');
+    setTimeout(() => {
+        document.body.removeChild(openedModal)
+    }, 410);
+}
